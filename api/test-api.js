36a56e64e67aefe285dec0bbd547ee6889b4598b
Rich Phillips <rphilindy@gmail.com>
2022-02-05 18:20:56 -0500

    commit

--- api/test-api.js
+++ api/test-api.js
@@ -5,8 +5,17 @@ var assert = require('assert');
 const urlBase="http://localhost:3001/";  
 
 
+const callApiMethod = async (endpoint) => {
+    const response = await fetch(`${urlBase}${endpoint}`);
+    const json = await response.json();
+    return json;
+}
+
 (async () => {
 
+    //these functions are written into the anonymous async function for ease of scripting/ clarity
+
+    //wait for the API to come alive when using as part of npm test (with concurrent)
     const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
     const waitForApi = async (timeout) => {
 
@@ -14,7 +23,7 @@ const urlBase="http://localhost:3001/";
         while((new Date()).getMilliseconds() < quitTime) {
             try {
                 await sleep(1000);
-                await fetch(`${urlBase}hello`);
+                await callApiMethod('hello');
                 return;
             }
             catch(e) {
@@ -23,15 +32,19 @@ const urlBase="http://localhost:3001/";
     }
     await waitForApi(30000);
 
+    
     console.log("Starting API Test");
 
+    
+    //hit the hello method as a sample
     const testHelloWorld = async () => {
-        const response = await fetch(`${urlBase}hello`);
-        const json = await response.json();
+        const json = await callApiMethod('hello');
         assert(json.message === 'hello');
     }
     await testHelloWorld();
 
+    
+    
     console.log("Completed API Test");
 
 })();
